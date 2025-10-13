9867500096ba447159649db52c2e4c8b42043a44
aripineninavya-rgb <aripineninavya@gmail.com>
2025-10-13 16:23:42 +0200

    first commit

---
+++ script/index.js
@@ -0,0 +1,36 @@
+
+console.log("EXERCISE 1:");
+console.log("Hello world");
+
+console.log("EXERCISE 2:");
+let firstName = "Navyasri";
+let lastName = "Aripineni";
+let age = "30";
+let favouriteColor = "Blue";
+
+console.log(`My first name is ${firstName}`);
+console.log(`My last name is ${lastName}`);
+console.log(`My age is ${age}`);
+console.log(`My favourite color is ${favouriteColor}`);
+
+
+console.log("EXERCISE 3:");
+
+let x = 450;
+let y = 50;
+
+add = x+y;
+subtract = x-y;
+multiply = x*y
+divide = x/y
+
+
+console.log(`x = ${x}`);
+console.log(`y = ${y}`);
+
+
+
+console.log(`The sum of x and y is ${add}`);
+console.log(subtract);
+console.log(`x*y = ${multiply}`);
+console.log(`The result is ${divide}`);
