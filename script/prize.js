610844684d80054971302a6626b5cde822c54638
aripineninavya-rgb <aripineninavya@gmail.com>
2025-10-17 22:26:24 +0200

    corrected according to Michelle instructions
    changed price variable instead of the originalPrice variable.
    
    added console on discount and actual price just to look clearly

--- script/prize.js
+++ script/prize.js
@@ -2,11 +2,22 @@ console.log("EXERCISE 2: Price tag");
 //console.log("Price tag");
 
 
-let originalPrice = "$49.99"
-console.log (`Actual price is ${originalPrice}`)
+let originalPrice = "$49.99";
+
 let price = prompt("Enter the price($49.99)");
-let oldPrice = parseFloat(originalPrice.slice(1));
+let oldPrice = parseFloat(price.slice(1));
+console.log (`Actual price is = $${oldPrice}`);
+
+let discount = "10%";
+console.log (`Discount = ${discount}`);
 
 
 let reducedPrice = oldPrice * 0.9;
-console.log (`Price after discount is $${reducedPrice}`);
+console.log (`Price after discount is = $${reducedPrice}`);
+
+
+
+
+
+
+
