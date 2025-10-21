e026c0c47cabbddd9db5e4de6f2451cb7a62b7e1
aripineninavya-rgb <aripineninavya@gmail.com>
2025-10-21 15:06:11 +0200

    verified email

---
+++ script/email.js
@@ -0,0 +1,14 @@
+console.log("assignment 3: Price tag");
+
+let username = prompt("Enter your email");
+
+function isValidEmail(username){
+    return username.includes("@gmail.com") ? "Valid email" : "invalid email";
+};
+
+console.log (isValidEmail(username));
+
+
+
+
+
